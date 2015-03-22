3b76fad023e4d9a93d1e75d42a16419d10007e91
Aad Versteden <madnificent@gmail.com>
2015-03-22 18:25:59 +0100

    Showcase custom request and response headers
    We will need to be add custom headers to the request and to the
    response.  This commit does exactly that.  We're showing that we can add
    custom request and response headers.
    
    Next up is finding out if we can somehow set some session variables.

--- lib/proxy.ex
+++ lib/proxy.ex
@@ -17,11 +17,22 @@ defmodule Proxy do
   def dispatch(conn, _opts) do
     # Start a request to the client saying we will stream the body.
     # We are simply passing all req_headers forward.
-    {:ok, client} = :hackney.request(:get, uri(conn), conn.req_headers, :stream, [])
+    req_headers = add_custom_request_headers( conn.req_headers )
+    {:ok, client} = :hackney.request(:get, uri(conn), req_headers, :stream, [])
 
     conn
     |> write_proxy(client)
     |> read_proxy(client)
+  end
+
+  # Adds custom headers to show we can do that trick.
+  defp add_custom_response_headers(headers) do
+    [ {"Magic-content", "42"} | headers ]
+  end
+
+  defp add_custom_request_headers(headers) do
+    IO.inspect headers
+    [ {"Api-version", "0.2.2"} | headers ]
   end
 
   # Reads the connection body and write it to the
@@ -48,7 +59,7 @@ defmodule Proxy do
     # if it is chunked or not and act accordingly to support streaming.
     #
     # We may also need to delete other headers in a proxy.
-    headers = List.keydelete(headers, "Transfer-Encoding", 1)
+    headers = add_custom_response_headers List.keydelete(headers, "Transfer-Encoding", 1)
 
     %{conn | resp_headers: headers}
     |> send_resp(status, body)
