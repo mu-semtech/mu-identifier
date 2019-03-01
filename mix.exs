17ade31ceb1c51237e8728507acafdaacf4b76bf
Aad Versteden <madnificent@gmail.com>
2019-03-01 10:29:44 +0100

    Release 1.6.0
    New features since 1.5.0:
    - DEFAULT_MU_AUTH_ALLOWED_GROUPS_HEADER
    - DEFAULT_ACCESS_CONTROL_ALLOW_ORIGIN_HEADER
    
    Fixes since 1.5.0:
    none

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Proxy.Mixfile do
 
   def project do
     [app: :proxy,
-     version: "1.5.0",
+     version: "1.6.0",
      elixir: "~> 1.0",
      deps: deps(),
      aliases: aliases()]
