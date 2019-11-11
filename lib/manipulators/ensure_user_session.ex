1f50ffcacf00353c345b49794938fcccadd7fd6a
Aad Versteden <madnificent@gmail.com>
2019-11-11 13:54:32 +0100

    Ensure session fetched, code style cleanup, and new plug_mint_proxy
    Some code style cleanup and ensuring the session is fetched before
    accessing it.  None of this should have a major impact.
    
    Along the side the plug_mint_proxy now lets us update the connection.
    This ensures we can keep the user's session id around.

--- lib/manipulators/ensure_user_session.ex
+++ lib/manipulators/ensure_user_session.ex
@@ -3,26 +3,29 @@ defmodule Manipulators.EnsureUserSession do
 
   @impl true
   def headers(headers, {frontend_connection, backend_connection}) do
+    frontend_connection = Plug.Conn.fetch_session(frontend_connection)
+
     frontend_connection =
       if Plug.Conn.get_session(frontend_connection, :proxy_user_id) do
-        IO.puts("keeping user_id")
         frontend_connection
       else
-        IO.puts("creating new rand user_id")
+        IO.puts("Creating new rand user_id")
+
+        new_user_id = "http://mu.semte.ch/sessions/" <> UUID.uuid1()
 
         Plug.Conn.put_session(
           frontend_connection,
           :proxy_user_id,
-          "http://mu.semte.ch/sessions/" <> UUID.uuid1()
+          new_user_id
         )
       end
 
-    {headers, {frontend_connection,backend_connection}}
+    {headers, {frontend_connection, backend_connection}}
   end
 
   @impl true
-  def chunk(_,_), do: :skip
+  def chunk(_, _), do: :skip
 
   @impl true
-  def finish(_,_), do: :skip
+  def finish(_, _), do: :skip
 end
--- lib/proxy.ex
+++ lib/proxy.ex
@@ -2,7 +2,8 @@ defmodule Proxy do
   use Plug.Builder
 
   @target "http://dispatcher/"
-  @encryption_salt Application.get_env(:proxy, :encryption_salt) || SecureRandom.urlsafe_base64(128)
+  @encryption_salt Application.get_env(:proxy, :encryption_salt) ||
+                     SecureRandom.urlsafe_base64(128)
   @signing_salt Application.get_env(:proxy, :signing_salt) || SecureRandom.urlsafe_base64(128)
 
   plug(Plug.Logger)
@@ -31,7 +32,7 @@ defmodule Proxy do
                 )
 
   def put_secret_key_base(conn, _) do
-    put_in conn.secret_key_base, Secret.secret_key_base()
+    put_in(conn.secret_key_base, Secret.secret_key_base())
   end
 
   def dispatch(conn, _opts) do
