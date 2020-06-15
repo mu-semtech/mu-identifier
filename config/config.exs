8ffe954f6989112c773e0cd8835fc6fe74ce34b0
Aad Versteden <madnificent@gmail.com>
2020-06-15 17:49:18 +0200

    Move calculate_same_site into CH module
    All calculations of headers had been fetched from there, best to move
    these in there to so some of them can be defined through defp.

--- config/config.exs
+++ config/config.exs
@@ -25,18 +25,18 @@ defmodule CH do
       _ -> false
     end
   end
-end
 
-defp calculate_same_site do
-  calculate_same_site(
-    System.get_env("DEFAULT_ACCESS_CONTROL_ALLOW_ORIGIN_HEADER"),
-    System.get_env("SESSION_COOKIE_SAME_SITE")
-  )
-end
+  def calculate_same_site do
+    calculate_same_site(
+      System.get_env("DEFAULT_ACCESS_CONTROL_ALLOW_ORIGIN_HEADER"),
+      System.get_env("SESSION_COOKIE_SAME_SITE")
+    )
+  end
 
-defp calculate_same_site(_, same_site) when is_binary(same_site), do: same_site
-defp calculate_same_site("*", nil), do: "None"
-defp calculate_same_site(nil, nil), do: "Lax"
+  defp calculate_same_site(_, same_site) when is_binary(same_site), do: same_site
+  defp calculate_same_site("*", nil), do: "None"
+  defp calculate_same_site(nil, nil), do: "Lax"
+end
 
 config :mu_identifier,
   encryption_salt: System.get_env("MU_ENCRYPTION_SALT"),
@@ -46,7 +46,7 @@ config :mu_identifier,
   default_mu_auth_allowed_groups_header: System.get_env("DEFAULT_MU_AUTH_ALLOWED_GROUPS_HEADER"),
   session_cookie_secure: CH.system_boolean("SESSION_COOKIE_SECURE", true),
   session_cookie_http_only: CH.system_boolean("SESSION_COOKIE_HTTP_ONLY", true),
-  session_cookie_same_site: calculate_same_site(),
+  session_cookie_same_site: CH.calculate_same_site(),
   log_allowed_groups: System.get_env("LOG_ALLOWED_GROUPS"),
   log_incoming_allowed_groups: System.get_env("LOG_INCOMING_ALLOWED_GROUPS"),
   log_outgoing_allowed_groups: System.get_env("LOG_OUTGOING_ALLOWED_GROUPS")
