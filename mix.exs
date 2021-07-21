3a664c8e4dce6e0babe71567f6db6958d567aa04
ajuvercr <arthur.vercruysse@outlook.com>
2021-07-21 20:22:29 +0200

    facilitate websocket traversal

--- mix.exs
+++ mix.exs
@@ -30,9 +30,12 @@ defmodule Proxy.Mixfile do
   # Type `mix help deps` for more examples and options
   defp deps do
     [{:plug_mint_proxy, git: "https://github.com/madnificent/plug-mint-proxy.git", tag: "v0.2.0"},
+      {:cowboy_ws_proxy, git: "https://github.com/ajuvercr/elixir-cowboy-ws-proxy-handler.git", tag: "v0.1"},
      {:uuid, "~> 1.1"},
+      {:gun, "~> 2.0.0-rc.2"},
      {:replug, "~> 0.1.0"},
      {:secure_random, "~> 0.5"},
+    {:exsync, "~> 0.2", only: :dev},
      {:observer_cli, "~> 1.5"}]
   end
 end
